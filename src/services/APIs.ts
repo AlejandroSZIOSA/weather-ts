b69c583df4785a538bcc3c74acde813333769528
AlejandroSZIOSA <ggato33@gmail.com>
2026-03-31 15:19:07 +0200

    connect API, private key env

--- src/services/APIs.ts
+++ src/services/APIs.ts
@@ -1,14 +1,30 @@
 import axios from "axios";
+import type { CurrentWeather } from "./APIs.types";
 
 /* const API_KEY = import.meta.env.VITE_OWN_APIKEY;
  */
+
+/* https://api.openweathermap.org/data/2.5/weather?appid=KEY=santiago
+ */
+
+const API_KEY = import.meta.env.VITE_OWN_APIKEY;
 
 const BASE_URL = "https://api.openweathermap.org/data/2.5/weather";
 const FAKE_SLOW_API = true;
 const FAKE_SLOW_API_DELAY = 2000;
 
 export const getCurrentWeather = async (city: string) => {
+  const res = await axios.get<CurrentWeather>(BASE_URL, {
+    params: {
+      q: city,
+      units: "metric",
+      appid: API_KEY,
+    },
+  });
+
   if (FAKE_SLOW_API) {
     await new Promise((resolve) => setTimeout(resolve, FAKE_SLOW_API_DELAY));
   }
+
+  return res.data;
 };
