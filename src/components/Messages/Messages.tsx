8a00f378ec90f9d5c73d191088f6797d65130f28
AlejandroSZIOSA <ggato33@gmail.com>
2026-04-05 21:27:32 +0200

    new component created

--- src/components/Messages/Messages.tsx
+++ src/components/Messages/Messages.tsx
@@ -1,15 +1,11 @@
-import React from "react";
-import styles from "./Messages.module.css";
+import { type FC, type ReactNode } from "react";
 
-interface MessagesProps {
-  variant: "error" | "success";
-  children: React.ReactNode;
-}
-
-export const Messages: React.FC<MessagesProps> = ({ variant, children }) => {
-  return (
-    <div className={`${styles["messagesContainer"]} ${styles[variant]}`}>
-      {children}
-    </div>
-  );
+type Props = {
+  children: ReactNode;
 };
+
+const Messages: FC<Props> = ({ children }) => {
+  return <div>{children}</div>;
+};
+
+export default Messages;
